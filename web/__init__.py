a2027f2af8a6c5bdc9ed7ae7c1a92dabe4860dd8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-10-02 14:41:10 +0200

    updated dependencies.
    Signed-off-by: Cédric Bonhomme <cedric@cedricbonhomme.org>

--- web/__init__.py
+++ web/__init__.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = os.urandom(25)
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 import web.views
--- web/views.py
+++ web/views.py
@@ -8,56 +8,58 @@ from stegano import lsb
 from web import app, allowed_file
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Returns the main page which will handle the hiding and revealing of
     the message.
     """
-    action = 'hide'
-    message = ''
+    action = "hide"
+    message = ""
 
-    if request.method == 'POST':
-        action = request.args.get('action', None)
+    if request.method == "POST":
+        action = request.args.get("action", None)
 
-        if action not in ('hide', 'reveal'):
-            flash('Action not specified.', 'danger')
-            return render_template('index.html')
+        if action not in ("hide", "reveal"):
+            flash("Action not specified.", "danger")
+            return render_template("index.html")
 
-        if 'file' not in request.files:
+        if "file" not in request.files:
             # check if the post request has the file part
-            flash('File is missing.', 'danger')
+            flash("File is missing.", "danger")
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files["file"]
 
-        if file.filename == '':
+        if file.filename == "":
             # if user does not select file, browser also
             # submit an empty part without filename
-            flash('File is missing.', 'danger')
+            flash("File is missing.", "danger")
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
             # an appropriate file has been submitted
             # now, checks the requested action
-            if 'reveal' == action:
+            if "reveal" == action:
                 # reveal the secret
                 message = lsb.reveal(file)
-            elif 'hide' == action:
+            elif "hide" == action:
                 # hide the secret
-                secret_to_hide = request.form.get('secret')
+                secret_to_hide = request.form.get("secret")
                 image = lsb.hide(file, secret_to_hide)
                 # convert the result to BytesIO
                 outputBytes = io.BytesIO()
                 image.save(outputBytes, "PNG")
                 outputBytes.seek(0)
                 # returns the image as an attachment
-                return send_file(outputBytes,
-                                    mimetype='image/png',
-                                    as_attachment=True,
-                                    attachment_filename='secret.png')
+                return send_file(
+                    outputBytes,
+                    mimetype="image/png",
+                    as_attachment=True,
+                    attachment_filename="secret.png",
+                )
         elif not allowed_file(file.filename):
-            flash('File type not allowed (only png or jpeg).', 'danger')
+            flash("File type not allowed (only png or jpeg).", "danger")
 
-    return render_template('index.html', action=action, message=message)
+    return render_template("index.html", action=action, message=message)
 
 
 @app.errorhandler(404)
@@ -66,8 +68,8 @@ def not_found(error=None):
     Handles 404 errors.
     """
     message = {
-            'status': 404,
-            'message': 'Not Found: ' + request.url,
+        "status": 404,
+        "message": "Not Found: " + request.url,
     }
     resp = jsonify(message)
     resp.status_code = 404
